5d86f282ae0d5aa8456722d6b450c1dc613ef46c
Savvas Kleanthous <skleanthous@outlook.com>
2019-03-19 14:52:28 +0000

    First version

---
+++ src/BullOak.Repositories.EntityFramework.Test.Integration/Contexts/ConfigurationContainer.cs
@@ -0,0 +1,36 @@
+﻿namespace BullOak.Repositories.EntityFramework.Test.Integration.Contexts
+{
+    using System;
+    using System.Reflection;
+    using BullOak.Repositories.Config;
+    using TechTalk.SpecFlow;
+
+    public class ConfigurationContainer
+    {
+        private static readonly string IdKey = Guid.NewGuid().ToString();
+        private readonly ScenarioContext scenarioContext;
+
+        public IHoldAllConfiguration Configuration
+        {
+            get => (IHoldAllConfiguration) scenarioContext[IdKey];
+            private set => scenarioContext[IdKey] = value;
+        }
+
+        public ConfigurationContainer(ScenarioContext scenarioContext)
+            => this.scenarioContext = scenarioContext;
+
+        public void Setup()
+            => Configuration = GetNewConfiguration();
+
+        public static IHoldAllConfiguration GetNewConfiguration()
+            => BullOak.Repositories.Configuration.Begin()
+                .WithDefaultCollection()
+                .WithDefaultStateFactory()
+                .NeverUseThreadSafe()
+                .WithNoEventPublisher()
+                .WithAnyAppliersFrom(Assembly.GetExecutingAssembly())
+                .AndNoMoreAppliers()
+                .WithNoUpconverters()
+                .Build();
+    }
+}
---
+++ src/BullOak.Repositories.EntityFramework.Test.Integration/Contexts/StateApplier.cs
@@ -0,0 +1,26 @@
+﻿namespace BullOak.Repositories.EntityFramework.Test.Integration.Contexts
+{
+    using System;
+    using BullOak.Repositories.Appliers;
+    using BullOak.Repositories.EntityFramework.Test.Integration.DbModel;
+
+    public class StateApplier : IApplyEvent<HoldHighOrders, MyEvent>
+    {
+        public HoldHighOrders Apply(HoldHighOrders state, MyEvent @event)
+        {
+            state.HigherOrder = Math.Max(@event.Order, state.HigherOrder);
+
+            return state;
+        }
+    }
+
+    public class InitializeEventApplier : IApplyEvent<HoldHighOrders, InitializeClientOrderEvent>
+    {
+        public HoldHighOrders Apply(HoldHighOrders state, InitializeClientOrderEvent @event)
+        {
+            state.ClientId = @event.ClientId.ToString();
+
+            return state;
+        }
+    }
+}
